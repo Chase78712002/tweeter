0bb7bf9ba50eed6a4cb282148083a2e96e190615
Alvin Lin <alvinch.lin@gmail.com>
2021-04-21 20:29:29 +0000

    Implement css styles for header, navigation bar, new tweet textarea, and tweet container

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+console.log("This is the char counter file!!");
+
+$(document).ready(() => {
+  // code..
+  console.log("DOM is now ready to be manipulated with jQuery");
+
+  let charCount = 0;
+  $("#tweet-text").on('input',function () {
+    const maxTweet = 140;
+    charCount = $(this).val().length;
+    let counter = $(this).siblings('div').children('output');
+    let diff = maxTweet - charCount;
+    if (diff < 0) {
+      counter.text(diff).addClass('overCount');
+    }
+    if (diff >= 0) {
+      counter.text(diff).removeClass('overCount');
+    }
+  });
+
+});
